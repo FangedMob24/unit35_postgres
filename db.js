f75a7f5c117e00c85a918ce38383195203385299
Hunter Powell <hunterp1510@gmail.com>
2025-12-05 15:53:06 -0600

    moved files and made changes to db.js

---
+++ db.js
@@ -0,0 +1,21 @@
+/** Database setup for BizTime. */
+
+const { Client } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+let DB_URI;
+
+if(process.env.NODE_ENV === "test") {
+    DB_URI = process.env.DATABASE_URL || "postgresql:///biztime_test";
+} else {
+    DB_URI = process.env.DATABASE_URL || "postgresql://biztime";
+}
+
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
